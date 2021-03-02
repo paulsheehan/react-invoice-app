46ed0dcdc7fad2f0bdf5599c66a3a31f96e8b431
paulsheehan <p.sheehan94@gmail.com>
2021-03-02 00:49:57 +0000

    Setup Redux for requesting local invoices

--- src/index.js
+++ src/index.js
@@ -1,14 +1,29 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom";
+import { Provider } from "react-redux";
+import { createStore, applyMiddleware, combineReducers } from "redux";
+import { createLogger } from "redux-logger";
+import thunkMiddleware from "redux-thunk";
+import "./index.scss";
+import App from "./containers/App";
+import reportWebVitals from "./reportWebVitals";
+import { searchRobots, requestLocalInvoices } from "./reducers";
+
+const logger = createLogger();
+
+const rootReducer = combineReducers({ searchRobots, requestLocalInvoices });
+const store = createStore(
+  rootReducer,
+  applyMiddleware(thunkMiddleware, logger)
+);
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Provider store={store}>
+      <App />
+    </Provider>
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById("root")
 );
 
 // If you want to start measuring performance in your app, pass a function
