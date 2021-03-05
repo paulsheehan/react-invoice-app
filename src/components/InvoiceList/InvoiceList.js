adf57820f2543f495b6d59ee5e53672d7de6ceea
paulsheehan <p.sheehan94@gmail.com>
2021-03-05 15:43:03 +0000

    Added empty styles

--- src/components/InvoiceList/InvoiceList.js
+++ src/components/InvoiceList/InvoiceList.js
@@ -2,6 +2,7 @@ import "./invoiceList.scss";
 import { ReactComponent as IconArrowDown } from "../../assets/icon-arrow-down.svg";
 import { ReactComponent as IconArrowRight } from "../../assets/icon-arrow-right.svg";
 import { ReactComponent as IconPlus } from "../../assets/icon-plus.svg";
+import { ReactComponent as EmptyIllustration } from "../../assets/illustration-empty.svg";
 import moment from "moment";
 
 const InvoiceList = (props) => {
@@ -25,41 +26,52 @@ const InvoiceList = (props) => {
           <span>New Invoice</span>
         </button>
       </div>
-      <ul className="invoice-list">
-        {props.invoices.map((invoice) => {
-          return (
-            <li className="invoice-list-item" key={invoice.id}>
-              <div className="invoice-list-item-value invoice-id bold">
-                <span className="hash">#</span>
-                {invoice.id}
-              </div>
-              <div className="invoice-list-item-value date light-text">
-                Due {moment(invoice.paymentDue).format("D MMM YYYY")}
-              </div>
-              <div className="invoice-list-item-value name light-text light-text--2">
-                {invoice.clientName}
-              </div>
-              <div className="invoice-list-item-value price">
-                £{invoice.total.toLocaleString()}
-              </div>
-              <div
-                className={
-                  "invoice-list-item-value invoice-list-status " +
-                  invoice.status
-                }
-              >
-                <div className="status-container">
-                  <span className="status-point" />
-                  <span className="status-text">{invoice.status}</span>
+      {props.invoices.length ? (
+        <ul className="invoice-list">
+          {props.invoices.map((invoice) => {
+            return (
+              <li className="invoice-list-item" key={invoice.id}>
+                <div className="invoice-list-item-value invoice-id bold">
+                  <span className="hash">#</span>
+                  {invoice.id}
                 </div>
-              </div>
-              <span className="invoice-list-item-value arrow">
-                <IconArrowRight />
-              </span>
-            </li>
-          );
-        })}
-      </ul>
+                <div className="invoice-list-item-value date light-text">
+                  Due {moment(invoice.paymentDue).format("D MMM YYYY")}
+                </div>
+                <div className="invoice-list-item-value name light-text light-text--2">
+                  {invoice.clientName}
+                </div>
+                <div className="invoice-list-item-value price">
+                  £{invoice.total.toLocaleString()}
+                </div>
+                <div
+                  className={
+                    "invoice-list-item-value invoice-list-status " +
+                    invoice.status
+                  }
+                >
+                  <div className="status-container">
+                    <span className="status-point" />
+                    <span className="status-text">{invoice.status}</span>
+                  </div>
+                </div>
+                <span className="invoice-list-item-value arrow">
+                  <IconArrowRight />
+                </span>
+              </li>
+            );
+          })}
+        </ul>
+      ) : (
+        <div className="invoice-list-empty">
+          <EmptyIllustration />
+          <h2 className="invoice-list-empty-title">There is nothing here</h2>
+          <p className="light-text">
+            Create an invoice by clicking the{" "}
+            <span className="bold">New Invoice</span> button and get started
+          </p>
+        </div>
+      )}
     </div>
   );
 };
